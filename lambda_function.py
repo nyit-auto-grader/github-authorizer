83c3ac75098e4313b139ea8da2d310869cd8c545
Rajan Khullar <rkhullar03@gmail.com>
2018-02-25 16:30:05 -0500

    switched to lambda proxy with request validator

--- lambda_function.py
+++ lambda_function.py
@@ -5,8 +5,9 @@ import json
 import os
 
 
-def build_response(isBase64Encoded=False, statusCode=200, headers=None, **kwargs):
-    return dict(isBase64Encoded=isBase64Encoded, statusCode=statusCode, headers=headers or dict(), body=json.dumps(kwargs))
+def build_response(isBase64Encoded=False, statusCode=200, headers=None, data=None, **kwargs):
+    body = json.dumps(dict(**(data or dict()), **kwargs))
+    return dict(isBase64Encoded=isBase64Encoded, statusCode=statusCode, headers=headers or dict(), body=body)
 
 
 def create_token(name: str='default'):
@@ -21,28 +22,12 @@ def github_login(username: str, password: str) -> bool:
     return response.status_code == 200
 
 
-# def lambda_handler(event, context):
-#     if event['httpMethod'] == 'POST':
-#         body = json.loads(event['body'])
-#         missing_fields = [field for field in ['username', 'password'] if field not in body]
-#         if len(missing_fields) > 0:
-#             return build_response(statusCode=400, message='missing required fields', fields=missing_fields)
-#
-#         if github_login(body['username'], body['password']):
-#             payload = create_token()
-#             return build_response(**payload)
-#         else:
-#             return build_response(statusCode=401, message='invalid username or password')
-#
-#     else:
-#         return build_response(statusCode=400, message='unsupported method')
-
-
 def lambda_handler(event, context):
-    if github_login(event['username'], event['password']):
-        return create_token()
+    body = json.loads(event['body'])
+    if github_login(body['username'], body['password']):
+        return build_response(data=create_token())
     else:
-        return dict(message="unauthorized")
+        return build_response(statusCode=404)
 
 
 if __name__ == '__main__':
